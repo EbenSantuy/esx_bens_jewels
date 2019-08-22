f40bcfc68b488ba19a9ef5452832302b49e6799a
EbenSantuy <50954825+EbenSantuy@users.noreply.github.com>
2019-08-22 07:39:15 +0000

    Add files via upload

---
+++ __resource.lua
@@ -0,0 +1,22 @@
+dependency 'essentialmode'
+
+client_scripts {
+	'@es_extended/locale.lua',
+	'locales/de.lua',
+	'locales/br.lua',
+	'locales/en.lua',
+	'locales/fr.lua',
+	'config.lua',
+	'client/esx_bens_jewels_cl.lua'
+}
+
+server_scripts {
+    '@mysql-async/lib/MySQL.lua',
+	'@es_extended/locale.lua',
+	'locales/de.lua',
+	'locales/br.lua',
+	'locales/en.lua',
+	'locales/fr.lua',
+	'config.lua',
+	'server/esx_bens_jewels_sv.lua'
+}
